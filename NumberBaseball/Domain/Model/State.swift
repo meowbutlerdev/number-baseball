a555ecca9cf24538e6964430c8a4054d714bdcdf
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-11 14:07:22 +0900

    fix: 정답 생성에 사용하는 숫자 범위 수정
    기존 1~9에서 0~9를 사용하도록 수정
      - 숫자는 0으로 시작하지 않도록 구현

--- NumberBaseball/Domain/Model/State.swift
+++ NumberBaseball/Domain/Model/State.swift
@@ -1,4 +1,4 @@
 struct State {
-    let numberRange = 1...9
+    let numberRange = 0...9
     let numberCount = 3
 }
--- NumberBaseball/Domain/UseCase/GenerateNumberUseCaseImpl.swift
+++ NumberBaseball/Domain/UseCase/GenerateNumberUseCaseImpl.swift
@@ -5,6 +5,10 @@ struct DefaultGenerateNumberUseCase: GenerateNumberUseCase {
     ///   - count: 사용할 숫자의 개수
     /// - Returns: 숫자 배열
     func numbers(range: ClosedRange<Int>, count: Int) -> [Int] {
-        return Array(range.shuffled().prefix(count))
+        let nonZero = range.filter { $0 != 0 }
+        guard let first = nonZero.randomElement() else { return [] }
+        let rest = range.filter { $0 != first }.shuffled().prefix(count - 1)
+
+        return [first] + rest
     }
 }
